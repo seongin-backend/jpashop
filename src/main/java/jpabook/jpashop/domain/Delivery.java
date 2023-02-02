bbd2c9994ce33e8ed161c37a609ed0f3254fcf5a
SEONGIN <alterin@naver.com>
2023-02-02 10:01:05 +0900

    오타 해결

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -9,9 +9,8 @@ import javax.persistence.*;
 @Getter @Setter
 public class Delivery {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "delivery id")
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
     private Long id;
 
     @OneToOne(mappedBy = "delivery")
