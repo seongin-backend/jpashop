d886473eebd0497e745e6deb39e42eebcad51064
SEONGIN <alterin@naver.com>
2023-04-26 11:13:24 +0900

    Getter 추가

--- src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+
+@Getter
 public class OrderSearch {
     private String memberName; //회원 이름
     private OrderStatus orderStatus; //주문상태[ORDER, CANCEL]
