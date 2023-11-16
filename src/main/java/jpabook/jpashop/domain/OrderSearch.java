073ef05595ddfc3156ed54c658649fb20a45b4bb
SEONGIN <alterin@naver.com>
2023-11-16 20:00:00 +0900

    주문 내역 페이지 검색조건이 OrderSearch 클래스의 Setter 어노테이션 없어서 안 되고 있었음. 그래서 추가

--- src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -1,8 +1,10 @@
 package jpabook.jpashop.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class OrderSearch {
     private String memberName; //회원 이름
     private OrderStatus orderStatus; //주문상태[ORDER, CANCEL]
